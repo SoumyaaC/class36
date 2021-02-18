0f473964bfeb274a63a18197530d04bb5f3602d9
Soumyaa26 <rachnachandarana27@gmail.com>
2021-02-18 18:48:48 +0530

    car racing game

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+var database;
+var gameState=0;
+var playerCount;
+var player, form, game;
+
+function setup(){
+    createCanvas(400, 400);
+    database = firebase.database();
+    game= new Game()
+    game.getState()
+    game.start()
+    
+}
+
+function draw(){
+  
+}
